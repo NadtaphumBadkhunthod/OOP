fb2c3f088ea7106ed4cad68d63fe722111d6535f
PluemBkt <68010366@kmitl.ac.th>
2026-03-09 06:58:33 +0700

    change add Staff

--- models/customers.py
+++ models/customers.py
@@ -175,8 +175,8 @@ class Member(Customer):
 
 class Staff(Member):
     count = 0
-    def __init__(self, name, surname, phonenumber, email, birth_month):
-        super().__init__(name, surname, phonenumber, email, birth_month)
+    def __init__(self, customer : Customer, birth_month):
+        super().__init__(customer, birth_month)
         self.__no_staff = f"STF-{Staff.count}"
 
     def info(self):
@@ -200,8 +200,8 @@ class Staff(Member):
         # Need implement
 
 class Manager(Staff):
-    def __init__(self, name, surname, phonenumber, email, birth_month, no_branch):
-        super().__init__(name, surname, phonenumber, email, birth_month, no_branch)
+    def __init__(self, customer : Customer, birth_month):
+        super().__init__(customer, birth_month)
     
     def print_report(self):
         pass
